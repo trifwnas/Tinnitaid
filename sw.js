2a56d338cb5748d756db3c5dd680a5ace91a4dc6
trif <trif.tz@gmail.com>
2019-01-19 23:07:53 +0100

    Min

--- sw.js
+++ sw.js
@@ -19,35 +19,35 @@ workbox.precaching.precacheAndRoute([
     "revision": "df128d26bb435e2363e54cb03a59117c"
   },
   {
-    "url": "android-chrome-128x128.png",
+    "url": "img/favicon/img/favicon/android-chrome-128x128.png",
     "revision": "931389ae9534e0116433ba245d7ccbd2"
   },
   {
-    "url": "android-chrome-144x144.png",
+    "url": "img/favicon/img/favicon/android-chrome-144x144.png",
     "revision": "fcbfa0e31cbe00db1e7e333b0b4085a9"
   },
   {
-    "url": "android-chrome-152x152.png",
+    "url": "img/favicon/android-chrome-152x152.png",
     "revision": "15c7666e3262c9a01198cc97e3dfeee2"
   },
   {
-    "url": "android-chrome-192x192.png",
+    "url": "img/favicon/android-chrome-192x192.png",
     "revision": "15bbfa9c5eda938db344c081acc21160"
   },
   {
-    "url": "android-chrome-384x384.png",
+    "url": "img/favicon/android-chrome-384x384.png",
     "revision": "435fa69761cfca1bdb521ee34e0b8dd7"
   },
   {
-    "url": "android-chrome-512x512.png",
+    "url": "img/favicon/android-chrome-512x512.png",
     "revision": "85456ca00d684bfe60a52c57b3416b8b"
   },
   {
-    "url": "android-chrome-72x72.png",
+    "url": "img/favicon/android-chrome-72x72.png",
     "revision": "3fe1e591c8f138f676c0b4d3f9eb58de"
   },
   {
-    "url": "android-chrome-96x96.png",
+    "url": "img/favicon/android-chrome-96x96.png",
     "revision": "3c0ded96a9d6cde35894280216bfb5d9"
   }
 ]);
