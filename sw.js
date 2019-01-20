c7bc1616033c318c2f09878e76c239481c6dbd13
trif <trif.tz@gmail.com>
2019-01-20 01:28:11 +0100

    Service Worker

--- sw.js
+++ sw.js
@@ -6,8 +6,96 @@
 									   )( 
 									  (__)
 */
-// TODO - update when Workbox v3 is published
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
+
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
+var precacheFiles = [
+      /* Add an array of files to precache for your app */
+      './', './index.html', './app.html', './offline.html', './css/style.css', './js/index.js'
+    ];
+
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function(evt) {
+    const urls = [
+      'https://cdn.ampproject.org/v0.js',
+      'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
+      'https://cdn.ampproject.org/shadow-v0.js',
+      'index.html',
+      '/'
+    ];
+    const cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(urls)));
+
+  console.log('The service worker is being installed.');
+  evt.waitUntil(precache().then(function() {
+    console.log('Skip waiting on install');
+    return self.skipWaiting();
+  }));
+});
+
+
+//allow sw to control of current page
+self.addEventListener('activate', function(event) {
+  console.log('Claiming clients for current page');
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.'+ evt.request.url);
+  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
+  evt.waitUntil(update(evt.request));
+});
+
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(precacheFiles);
+  });
+}
+
+function fromCache(request) {
+  //we pull files from the cache first thing so we can show them fast
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  //this is where we call the server to get the newest version of the 
+  //file to use the next time we show view
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+function fromServer(request){
+  //this is the fallback if it is not in the cache to go to the server and get it
+  return fetch(request).then(function(response){ return response});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([
   {
@@ -80,3 +168,4 @@ workbox.routing.registerRoute(/\.(?:js|css|png|gif|jpg|svg)$/,
 workbox.routing.registerRoute(/(.*)cdn\.ampproject\.org(.*)/,
   workbox.strategies.staleWhileRevalidate()
 );
+ */
