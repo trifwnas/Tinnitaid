5f404d9cbc97c30f96ffcf5d32836a0b96e8e9f2
trif <trif.tz@gmail.com>
2020-01-26 01:20:28 +0100

    Service worker bug fixed

--- sw.js
+++ sw.js
@@ -1,24 +1,17 @@
 // Service Worker with Cache-first network
 
-var CACHE = "stopTinnitusPrecache";
+var cacheName = "stopTinnitusPrecache";
 var precacheFiles = [
   /* Array of files to precache */
-  // '/',
-  "/labs/stoptinnitus/",
   "/labs/stoptinnitus/index.html",
-  "/labs/stoptinnitus/app.html",
-  "/labs/stoptinnitus/therapy.html",
-  "/labs/stoptinnitus/src/scripts/app.js",
-  "/labs/stoptinnitus/src/scripts/main.js",
-  "/labs/stoptinnitus/src/scripts/therapy.js",
-  "/labs/stoptinnitus/src/scripts/workbox-config.js",
-  "/labs/stoptinnitus/src/img"
+  "/labs/stoptinnitus/detect.html",
+  "/labs/stoptinnitus/therapy.html"
 ];
 
 //Install stage sets up the cache-array to configure pre-cache content
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function(event) {
   console.log("The service worker is being installed.");
-  evt.waitUntil(
+  event.waitUntil(
     precache().then(function() {
       console.log("Skip waiting on install");
       return self.skipWaiting();
@@ -27,26 +20,26 @@ self.addEventListener("install", function(evt) {
 });
 
 // Allow sw to control current page
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function(event) {
   console.log("Claiming clients for current page");
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function(evt) {
-  console.log("The service worker is serving the asset." + evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
+self.addEventListener("fetch", function(event) {
+  console.log("The service worker is serving the asset." + event.request.url);
+  event.respondWith(fromCache(event.request).catch(fromServer(event.request)));
+  event.waitUntil(update(event.request));
 });
 
 function precache() {
-  return caches.open(CACHE).then(function(cache) {
+  return caches.open(cacheName).then(function(cache) {
     return cache.addAll(precacheFiles);
   });
 }
 
 function fromCache(request) {
   //we pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(function(cache) {
+  return caches.open(cacheName).then(function(cache) {
     return cache.match(request).then(function(matching) {
       return matching || Promise.reject("no-match");
     });
@@ -56,7 +49,7 @@ function fromCache(request) {
 function update(request) {
   //this is where we call the server to get the newest version of the
   //file to use the next time we show view
-  return caches.open(CACHE).then(function(cache) {
+  return caches.open(cacheName).then(function(cache) {
     return fetch(request).then(function(response) {
       return cache.put(request, response);
     });
