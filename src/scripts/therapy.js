258d64fe514682d77f4b8f12e997288a640ac69c
trif <trif.tz@gmail.com>
2020-05-17 22:52:35 +0200

    Change buffer to 1 hour

--- src/scripts/therapy.js
+++ src/scripts/therapy.js
@@ -28,6 +28,124 @@ The creating team of the app does not hold any responsibility on how the app is 
 For the complete license, please refer here: http://tafhub.com/labs/stoptinnitus/LICENSE.txt
 ************************************************************************
  */
+
+//...................................................... live working
+// *** NOISE GENERATOR SCRIPT ***
+
+// Create Web Audio API context | Temp workaround until AudioContext is standardized
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
+numSeconds = 3600;
+sampleRate = 32000;
+bufferSize = numSeconds * sampleRate;
+
+var whiteNoiseNode = context.createBufferSource(),
+  buffer = context.createBuffer(1, bufferSize, sampleRate), // context.sampleRate
+  data = buffer.getChannelData(0);
+for (var i = 0; i < bufferSize; i++) {
+  data[i] = Math.random() * 2 - 1;
+}
+whiteNoiseNode.buffer = buffer;
+whiteNoiseNode.loop = true;
+
+var connected = false;
+
+// Volume
+var gainNode = context.createGain();
+gainNode.gain.value = 0.2; // 20 %
+gainNode.connect(context.destination);
+
+var playStopWhiteNoise = function() {
+  if (!connected) {
+    whiteNoiseNode.connect(gainNode);
+
+    if (iOS) {
+      alert(
+        "Sound started. Un-mute your device or select volume if you cannot hear anything."
+      );
+      webAudioTouchUnlock(context).then(
+        function(unlocked) {
+          if (unlocked) {
+            // AudioContext was unlocked from an explicit user action,
+            // sound should start playing now
+            whiteNoiseNode.start(context.currentTime);
+          } else {
+            alert("Restart is needed");
+            // window.location = 'http://127.0.0.1:8881/therapy.html'; // testing
+            window.location = "https://www.tafhub.com/labs/stoptinnitus/therapy.html";
+          }
+        },
+        function(reason) {
+          console.error(reason);
+        }
+      );
+    } else {
+      // Non-iOS
+      whiteNoiseNode.start(context.currentTime);
+    }
+  } else {
+    // If connected
+    if (iOS) {
+      whiteNoiseNode.stop(0);
+    } else {
+      // Non-iOS Sound off
+      whiteNoiseNode.stop(0);
+      whiteNoiseNode.disconnect();
+      // window.location = 'http://127.0.0.1:8881/therapy.html'; // testing
+      window.location = "https://www.tafhub.com/labs/stoptinnitus/therapy.html"; //production
+    }
+    gainNode.disconnect();
+  }
+  connected = !connected;
+};
+
+// *** //NOISE GENERATOR SCRIPT ***
+
+// *** iOS CHECK SCRIPT ***
+var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+if (isSafari && iOS) {
+  alert(
+    "You are using Safari on iOS! The feature of listening to your music in parallel with the generated sound is not allowed in this browser. Please use Chrome."
+  );
+}
+// *** //iOS CHECK SCRIPT ***
+
+// *** iOS CHECK TOUCH SCRIPT ***
+function webAudioTouchUnlock(context) {
+  return new Promise(function(resolve, reject) {
+    if (context.state === "suspended" && "ontouchstart" in window) {
+      var unlock = function() {
+        context.resume().then(
+          function() {
+            document.body.removeEventListener("touchstart", unlock);
+            document.body.removeEventListener("touchend", unlock);
+
+            resolve(true);
+          },
+          function(reason) {
+            reject(reason);
+          }
+        );
+      };
+
+      document.body.addEventListener("touchstart", unlock, false);
+      document.body.addEventListener("touchend", unlock, false);
+    } else {
+      resolve(false);
+    }
+  });
+}
+// *** //iOS CHECK TOUCH SCRIPT ***
+
+// *** VOLUME SCRIPT ***
+var setWhiteNoiseVolume = function() {
+  var vol = document.getElementById("whiteNoiseVolRange").value;
+  gainNode.gain.value = vol;
+};
+// *** //VOLUME SCRIPT ***
+
 /* 
 // *** NOISE GENERATOR SCRIPT ***
 // Create Web Audio API context | Temp workaround until AudioContext is standardized
@@ -258,120 +376,3 @@ button.onclick = function() {
 };
 
 // *** //VOLUME SCRIPT *** */
-
-//...................................................... live working
-// *** NOISE GENERATOR SCRIPT ***
-
-// Create Web Audio API context | Temp workaround until AudioContext is standardized
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const context = new AudioContext();
-//bufferSize = 4096;
-bufferSize = 84100;
-
-var whiteNoiseNode = context.createBufferSource(),
-  buffer = context.createBuffer(1, bufferSize, context.sampleRate),
-  data = buffer.getChannelData(0);
-for (var i = 0; i < bufferSize; i++) {
-  data[i] = Math.random() * 2 - 1;
-}
-whiteNoiseNode.buffer = buffer;
-whiteNoiseNode.loop = true;
-
-var connected = false;
-
-// Volume
-var gainNode = context.createGain();
-gainNode.gain.value = 0.2; // 20 %
-gainNode.connect(context.destination);
-
-var playStopWhiteNoise = function() {
-  if (!connected) {
-    whiteNoiseNode.connect(gainNode);
-
-    if (iOS) {
-      alert(
-        "Sound started. Un-mute your device or select volume if you cannot hear anything."
-      );
-      webAudioTouchUnlock(context).then(
-        function(unlocked) {
-          if (unlocked) {
-            // AudioContext was unlocked from an explicit user action,
-            // sound should start playing now
-            whiteNoiseNode.start(context.currentTime);
-          } else {
-            alert("Restart is needed");
-            // window.location = 'http://127.0.0.1:8881/therapy.html'; // testing
-            window.location =
-              "https://www.tafhub.com/labs/stoptinnitus/therapy.html";
-          }
-        },
-        function(reason) {
-          console.error(reason);
-        }
-      );
-    } else {
-      // Non-iOS
-      whiteNoiseNode.start(context.currentTime);
-    }
-  } else {
-    // If connected
-    if (iOS) {
-      whiteNoiseNode.stop(0);
-    } else {
-      // Non-iOS Sound off
-      whiteNoiseNode.stop(0);
-      whiteNoiseNode.disconnect();
-      // window.location = 'http://127.0.0.1:8881/therapy.html'; // testing
-      window.location = "https://www.tafhub.com/labs/stoptinnitus/therapy.html"; //production
-    }
-    gainNode.disconnect();
-  }
-  connected = !connected;
-};
-
-// *** //NOISE GENERATOR SCRIPT ***
-
-// *** iOS CHECK SCRIPT ***
-var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
-var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-if (isSafari && iOS) {
-  alert(
-    "You are using Safari on iOS! The feature of listening to your music in parallel with the generated sound is not allowed in this browser. Please use Chrome."
-  );
-}
-// *** //iOS CHECK SCRIPT ***
-
-// *** iOS CHECK TOUCH SCRIPT ***
-function webAudioTouchUnlock(context) {
-  return new Promise(function(resolve, reject) {
-    if (context.state === "suspended" && "ontouchstart" in window) {
-      var unlock = function() {
-        context.resume().then(
-          function() {
-            document.body.removeEventListener("touchstart", unlock);
-            document.body.removeEventListener("touchend", unlock);
-
-            resolve(true);
-          },
-          function(reason) {
-            reject(reason);
-          }
-        );
-      };
-
-      document.body.addEventListener("touchstart", unlock, false);
-      document.body.addEventListener("touchend", unlock, false);
-    } else {
-      resolve(false);
-    }
-  });
-}
-// *** //iOS CHECK TOUCH SCRIPT ***
-
-// *** VOLUME SCRIPT ***
-var setWhiteNoiseVolume = function() {
-  var vol = document.getElementById("whiteNoiseVolRange").value;
-  gainNode.gain.value = vol;
-};
-// *** //VOLUME SCRIPT ***
